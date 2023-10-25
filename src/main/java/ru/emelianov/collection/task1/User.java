9e9ac254b69910c2fd48eb67cf6b32a0c47f9ed9
Mikhail Emelianov <cheburusshka0303@gmail.com>
2023-10-25 22:22:32 +0500

    Initial Commit

---
+++ src/main/java/ru/emelianov/collection/task1/User.java
@@ -0,0 +1,13 @@
+package ru.emelianov.collection.task1;
+
+/**
+ * Пользователь
+ *
+ * @author mikhailemv
+ * @since 25.10.2023
+ */
+public class User {
+    private String username;
+    private String email;
+    private byte[] passwordHash;
+}
---
+++ src/main/java/ru/emelianov/collection/task2/Ticket.java
@@ -0,0 +1,12 @@
+package ru.emelianov.collection.task2;
+
+/**
+ * Билет
+ *
+ * @author mikhailemv
+ * @since 25.10.2023
+ */
+public class Ticket {
+    private long id;
+    private String client;
+}
