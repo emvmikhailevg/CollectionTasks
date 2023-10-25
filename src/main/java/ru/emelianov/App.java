ef5315ca655c84d40dd839d763ba33dc65adba17
Mikhail Emelianov <cheburusshka0303@gmail.com>
2023-10-25 22:59:18 +0500

    Start Commit

--- src/main/java/ru/emelianov/App.java
+++ src/main/java/ru/emelianov/App.java
@@ -1,48 +1,13 @@
 package ru.emelianov;
 
-import ru.emelianov.collection.task1.User;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import static ru.emelianov.collection.task1.Task1.findDuplicates;
-
 /**
- * Main class for test tasks
+ * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        testFirstTask();
-    }
-
-    private static void testFirstTask() {
-        // Создаем несколько пользователей
-        User user1 = new User("user1", "user1@example.com", new byte[] { 1, 2, 3 });
-        User user2 = new User("user2", "user2@example.com", new byte[] { 4, 5, 6 });
-        User user3 = new User("user3", "user3@example.com", new byte[] { 7, 8, 9 });
-        User user4 = new User("user1", "user1@example.com", new byte[] { 1, 2, 3 }); // Дубликат
-
-        // Создаем коллекции
-        Collection<User> collA = new ArrayList<>();
-        collA.add(user1);
-        collA.add(user2);
-        collA.add(user3);
-
-        Collection<User> collB = new ArrayList<>();
-        collB.add(user2);
-        collB.add(user3);
-        collB.add(user4);
-
-        // Вызываем метод findDuplicates
-        List<User> duplicates = findDuplicates(collA, collB);
-
-        // Выводим дубликаты
-        for (User user : duplicates) {
-            System.out.println("Duplicate User: " + user.getUsername() + ", " + user.getEmail());
-        }
+        System.out.println( "Hello World!" );
     }
 }
--- src/main/java/ru/emelianov/collection/task1/Task1.java
+++ src/main/java/ru/emelianov/collection/task1/Task1.java
@@ -29,20 +29,36 @@ import java.util.*;
  * @author mikhailemv
  * @since 25.10.2023
  */
+/**
+ * Дано:
+ * <pre>
+ * public class User {
+ *     private String username;
+ *     private String email;
+ *     private byte[] passwordHash;
+ *     …
+ * }
+ * </pre>
+ * Нужно написать утилитный метод
+ * <pre>
+ * public static List<User> findDuplicates(Collection<User> collA, Collection<User> collB);
+ * </pre>
+ * <p>который возвращает дубликаты пользователей, которые есть в обеих коллекциях.</p>
+ * <p>Одинаковыми считаем пользователей, у которых совпадают все 3 поля: username,
+ * email, passwordHash. Дубликаты внутри коллекций collA, collB можно не учитывать.</p>
+ * <p>Метод должен быть оптимален по производительности.</p>
+ * <p>Пользоваться можно только стандартными классами Java SE.
+ * Коллекции collA, collB изменять запрещено.</p>
+ *
+ * См. {@link User}
+ *
+ * @author mikhailemv
+ * @since 25.10.2023
+ */
 public class Task1 {
 
     public static List<User> findDuplicates(Collection<User> collA, Collection<User> collB) {
-        Set<User> setA = new HashSet<>(collA);
-        Set<User> setB = new HashSet<>(collB);
-
-        List<User> duplicates = new ArrayList<>();
-
-        for (User userA : setA) {
-            if (setB.contains(userA)) {
-                duplicates.add(userA);
-            }
-        }
-
-        return duplicates;
+        // TODO
+        return null;
     }
 }
